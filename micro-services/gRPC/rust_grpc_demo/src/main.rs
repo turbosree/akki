6efc0e384518a74dc119aa71cf625517e741a3a7
Sreejith Naarakathil <sreejith.naarakathil@gmail.com>
2022-08-21 16:15:24 +0200

    gRPC srv demo deployment in k8s using ingress and its testing methods

---
+++ micro-services/gRPC/rust_grpc_demo/src/main.rs
@@ -0,0 +1,59 @@
+use tonic::{transport::Server, Request, Response, Status};
+// use tonic_reflection;
+
+use bookstore::bookstore_server::{Bookstore, BookstoreServer};
+use bookstore::{GetBookRequest, GetBookResponse};
+
+
+mod bookstore {
+    include!("bookstore.rs");
+
+    // Add this
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("greeter_descriptor");
+}
+
+
+#[derive(Default)]
+pub struct BookStoreImpl {}
+
+#[tonic::async_trait]
+impl Bookstore for BookStoreImpl {
+    async fn get_book(
+        &self,
+        request: Request<GetBookRequest>,
+    ) -> Result<Response<GetBookResponse>, Status> {
+        println!("Request from {:?}", request.remote_addr());
+
+        let response = GetBookResponse {
+            id: request.into_inner().id,
+            author: "Peter".to_owned(),
+            name: "Zero to One".to_owned(),
+            year: 2014,
+        };
+        Ok(Response::new(response))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // let addr = "[::1]:50051".parse().unwrap();
+    let addr = "0.0.0.0:50051".parse().unwrap();
+    let bookstore = BookStoreImpl::default();
+
+    // Add this
+    let reflection_service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(bookstore::FILE_DESCRIPTOR_SET)
+        .build()
+        .unwrap();
+
+    println!("Bookstore server listening on {}", addr);
+
+    Server::builder()
+        .add_service(BookstoreServer::new(bookstore))
+        .add_service(reflection_service) // Add this
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
